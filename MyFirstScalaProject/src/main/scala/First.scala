8c792d123d61495b023db69b6aad145fd9155bbc
YuvanRaj <yuviram91@gmail.com>
2019-02-28 23:49:27 +0530

    My First Scala Project Check in

---
+++ MyFirstScalaProject/src/main/scala/First.scala
@@ -0,0 +1,7 @@
+package main.scala
+
+object First {
+  def main(args: Array[String]) {
+      println("Hello, world!")
+    }
+}
---
+++ MyFirstScalaProject/src/main/scala/Second.scala
@@ -0,0 +1,7 @@
+package main.scala
+
+object Second {
+  def main(args: Array[String]) {
+      println("Second")
+    }
+}
